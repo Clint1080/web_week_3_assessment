d4ebbd6118c71bb3d73225e0be7f5a4d7bb31b34
Clint Edwards <cedwards1080@gmail.com>
2021-08-06 09:49:43 -0600

    Part 2

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	window.alert(`Form submission successful!`)
 }
 
 
 let form = document.querySelector('form#contact');
 
+const goodUser = (e) => {
+	e.preventDefault()
+
+	window.alert(`Wow you look hot today ;)`)
+}
+let img = document.querySelector(`img`)
+
+img.addEventListener(`mouseover`, goodUser);
 form.addEventListener('submit', handleSubmit);
